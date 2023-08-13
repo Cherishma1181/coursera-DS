cae48fc478ddcd0e4c20cdf633fc26de398aba8a
Cherishma1181 <114346488+Cherishma1181@users.noreply.github.com>
2023-08-13 22:20:51 +0530

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,50 @@
+<h1>Data Science Tools and Ecosystem<h1>
+
+In this notebook, Data Science Tools and Ecosystem are summarized.
+
+Some of the popular languages that Data Scientists use are:-
+<ol>
+<li>Python</li>
+<li>Java</li>
+<li>R</li>
+</ol>
+
+ Some of the commonly used libraries used by Data Scientists include:-
+<ol>
+<li>Matplotlib</li>
+<li>Pandas</li>
+<li>Numpy</li>
+</ol>
+
+ <table>
+<tr>
+<th>Data Science Tools:</th>
+</tr>
+
+<tr>
+<th>RStudio</th>
+<th>Apache Spark</th>
+<th>TensorFlow</th>
+</tr>
+</table>
+
+ <h3>Below are a few examples of evaluating arithmetic expressions in Python<h3>
+
+This a simple arithmetic expression to mutiply then add integers:
+result = (3 * 4) + 5
+print(result)
+
+This will convert 200 minutes to hours by diving by 60:
+minutes=200
+hours=minutes/60
+print(hours)
+
+__Objectives:__
+<ol>
+<li>List popular languages for Data Science</li>
+<li>List commonly used libraries used by Data Scientists</li>
+<li>List the Data Science tools</li>
+</ol>
+
+<h2>Author</h2>
+ABC
